bd16a774d17dcdbb194f2331c31efab15cfb84f8
jlamadri <jlamadri@amazon.com>
2020-01-16 12:31:21 -0600

    lab updates

---
+++ README.md
@@ -0,0 +1,6 @@
+# About
+
+
+
+# Reference
+- Create serverless social Android App: https://master.d3nqjwfto6b0fr.amplifyapp.com/
--- app/src/main/java/com/example/socialandroidapp/SignUpActivity.java
+++ app/src/main/java/com/example/socialandroidapp/SignUpActivity.java
@@ -59,17 +59,96 @@ public class SignUpActivity extends FragmentActivity
         this.password = password;
 
         // Add code here
+        final Map<String, String> attributes = new HashMap<>();
+        attributes.put("email", email);
+        AWSMobileClient.getInstance().signUp(userName, password, attributes, null, new Callback<SignUpResult>() {
+            @Override
+            public void onResult(final SignUpResult signUpResult) {
+                runOnUiThread(() -> {
+                    if (!signUpResult.getConfirmationState()) {
+                        final UserCodeDeliveryDetails details = signUpResult.getUserCodeDeliveryDetails();
+                        makeToast(context, "Confirm sign-up with: " + details.getDestination());
+                        setSignUpConfirmFragment();
+                    } else {
+                        makeToast(context, "Sign-up done.");
+                    }
+                });
+            }
 
+            @Override
+            public void onError(Exception e) {
+                Log.e(TAG, "Sign-up error", e);
+                runOnUiThread(() -> {
+                    if (e instanceof AmazonServiceException)
+                        makeToast(context, ((AmazonServiceException) e).getErrorMessage());
+                });
+            }
+        });
     }
 
     @Override
     public void confirmSignUp(String code) {
         // Add code here
+        AWSMobileClient.getInstance().confirmSignUp(userName, code, new Callback<SignUpResult>() {
+            @Override
+            public void onResult(final SignUpResult signUpResult) {
+                runOnUiThread(() -> {
+                    Log.d(TAG, "Sign-up callback state: " + signUpResult.getConfirmationState());
+                    if (!signUpResult.getConfirmationState()) {
+                        final UserCodeDeliveryDetails details = signUpResult.getUserCodeDeliveryDetails();
+                        makeToast(context,"Confirm sign-up with: " + details.getDestination());
+                    } else {
+                        makeToast(context, "Sign-up done.");
+                        // SignIn and move to MainActivity
+                        _signIn(userName, password);
+                    }
+                });
+            }
 
+            @Override
+            public void onError(Exception e) {
+                Log.e(TAG, "Confirm sign-up error", e);
+                runOnUiThread(() -> {
+                    if (e instanceof AmazonServiceException)
+                        makeToast(context, ((AmazonServiceException) e).getErrorMessage());
+                });
+            }
+        });
     }
 
     private void _signIn(String username, String password) {
         // Add code here
+        AWSMobileClient.getInstance().signIn(username, password, null, new Callback<SignInResult>() {
+            @Override
+            public void onResult(final SignInResult signInResult) {
+                runOnUiThread(() -> {
+                    Log.d(TAG, "Sign-in callback state: " + signInResult.getSignInState());
+                    switch (signInResult.getSignInState()) {
+                        case DONE:
+                            makeToast(context, "Sign-in done.");
+                            CommonAction.openMain(context);
+                            break;
+                        case SMS_MFA:
+                            makeToast(context, "Please confirm sign-in with SMS.");
+                            break;
+                        case NEW_PASSWORD_REQUIRED:
+                            makeToast(context, "Please confirm sign-in with new password.");
+                            break;
+                        default:
+                            makeToast(context, "Unsupported sign-in confirmation: " + signInResult.getSignInState());
+                            break;
+                    }
+                });
+            }
 
+            @Override
+            public void onError(Exception e) {
+                Log.e(TAG, "Sign-in error", e);
+                runOnUiThread(() -> {
+                    if (e instanceof AmazonServiceException)
+                        makeToast(context, ((AmazonServiceException) e).getErrorMessage());
+                });
+            }
+        });
     }
 }
